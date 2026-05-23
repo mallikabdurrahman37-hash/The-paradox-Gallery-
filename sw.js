e6c62d6b46abbc3c31196378bd89f546638d8f6c
mallikabdurrahman37-hash <mallikabdurrahman37@gmail.com>
2026-05-23 21:54:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
 const CACHE_NAME = "paradox-gallery-v1";
+
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/css/global.css",
-  "/css/gallery.css"
+  "/The-paradox-Gallery-/",
+  "index.html",
+  "css/global.css",
+  "css/gallery.css"
 ];
 
 // Install Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log("Cache Opened");
       return cache.addAll(urlsToCache);
     })
   );
@@ -20,6 +22,23 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate & Clean Old Cache
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
